c9877afe830d9066ce3103eae5ce351efba5802d
Sandra Gury <sgury@student.42.fr>
2019-04-22 10:50:12 +0200

    OK

--- get_next_line.c
+++ get_next_line.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   gnl_new_version.c                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sgury <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/04/11 17:06:12 by sgury             #+#    #+#             */
-/*   Updated: 2019/04/17 10:14:02 by sgury            ###   ########.fr       */
+/*   Created: 2019/04/19 13:26:19 by sgury             #+#    #+#             */
+/*   Updated: 2019/04/19 16:51:57 by sgury            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,66 +19,74 @@ static int	ft_line_len(char *content)
 	len = 0;
 	while (content[len] != '\n' && content[len] != '\0')
 		len++;
-	if (len == 0 && content[len] == '\n')
-		return (1);
-	if (content[len] == '\n')
+	if (content[len] == '\n' || content[len] == '\0')
 		return (len);
 	return (-1);
 }
 
-static int	ft_get_new_content(const int fd, char **content)
+static int	ft_get_new_line(char **line, char **content, int fd, int len)
+{
+	char	*tmp;
+
+	if (content[fd][len] == '\n')
+	{
+		if ((*line = ft_strsub(content[fd], 0, len)) == NULL)
+			return (-1);
+		if ((tmp = ft_strdup(content[fd] + len + 1)) == NULL)
+			return (-1);
+		free(content[fd]);
+		content[fd] = tmp;
+	}
+	else
+	{
+		if ((*line = ft_strdup(content[fd])) == NULL)
+			return (-1);
+		ft_strdel(&content[fd]);
+	}
+	return (1);
+}
+
+static int	ft_add_new_content(int fd, char **content)
 {
 	char	buff[BUFF_SIZE + 1];
 	char	*tmp;
 	int		ret;
 
-	ret = read(fd, buff, BUFF_SIZE);
-	if (ret < 0)
-		return (ret);
-	buff[BUFF_SIZE] = '\0';
-	if ((tmp = ft_strjoin(*content, buff)) == NULL)
-		return (-1);
-	*content = tmp;
+	ret = 1;
+	while ((ret = read(fd, buff, BUFF_SIZE)) > 0)
+	{
+		buff[ret] = '\0';
+		if ((tmp = ft_strjoin(content[fd], buff)) == NULL)
+			return (-1);
+		free(content[fd]);
+		content[fd] = tmp;
+		if (ft_strchr(content[fd], '\n') > 0)
+			return (ret);
+	}
 	return (ret);
-}
-
-static char	*ft_get_new_line(char *content, char *line, int len)
-{
-	char	*tmp;
-
-	if ((tmp = ft_strnew(len)) == NULL)
-		return (NULL);
-	ft_memmove(tmp, content, len);
-	tmp[len] = '\0';
-	line = tmp;
-	ft_strdel(&tmp);
-	return (line);
 }
 
 int			get_next_line(const int fd, char **line)
 {
-	static char	*content;
-	char		*tmp;
+	static char	*content[FD_MAX];
 	int			line_len;
-	int			buff_ret;
+	int			read_ret;
 
 	line_len = 0;
-	buff_ret = 0;
-	if (line == NULL || fd < 0 || BUFF_SIZE < 1 || fd > FD_MAX)
+	read_ret = 1;
+	if (line == NULL || BUFF_SIZE < 1 || fd < 0 || fd > FD_MAX)
 		return (-1);
-	if (content == NULL)
-		buff_ret = ft_get_new_content(fd, &content);
-	while ((line_len = ft_line_len(content)) < 1)
-		if ((buff_ret = ft_get_new_content(fd, &content)) == 0)
-			break ;
-	if (buff_ret < 0)
-		return (-1);
-	if (buff_ret == 0 && line_len < 1)
-		return (0);
-	if (content != NULL)
-		if ((*line = ft_get_new_line(content, *line, line_len)) == NULL)
+	if (content[fd] == NULL)
+		if ((content[fd] = ft_strnew(1)) == NULL)
 			return (-1);
-	tmp = ft_strchr(content, '\n');
-	content = tmp + 1;
-	return (1);
+	line_len = ft_line_len(content[fd]);
+	if (content[fd][line_len] == '\n')
+		return (ft_get_new_line(line, content, fd, line_len));
+	read_ret = ft_add_new_content(fd, content);
+	if (read_ret < 0)
+		return (-1);
+	if (read_ret == 0 && content[fd][0] == '\0')
+		return (0);
+	line_len = ft_line_len(content[fd]);
+	return (ft_get_new_line(line, content, fd, line_len));
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: sgury <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/11 17:07:57 by sgury             #+#    #+#             */
-/*   Updated: 2019/04/17 10:01:03 by sgury            ###   ########.fr       */
+/*   Updated: 2019/04/19 16:54:30 by sgury            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,10 @@
 
 # include <unistd.h>
 # include <stdlib.h>
-# define BUFF_SIZE 100
+# include "./libft/libft.h"
+# define BUFF_SIZE  100
 # define FD_MAX 4864
 
 int		get_next_line(const int fd, char **line);
-char	*ft_strjoin(char const *s1, char const *s2);
-char	*ft_strnew(size_t size);
-void	*ft_memmove(void *dst, const void *src, size_t len);
-char	*ft_strchr(const char *s, int c);
-void	ft_strdel(char **as);
 
 #endif
